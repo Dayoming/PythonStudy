7d71e7049ca76e6e77a3bfb06ccaceed38c39580
Dayoming <jdayoung1204@naver.com>
2018-07-17 23:52:01 +0900

    Unit 7. 문자열 사용하기

---
+++ codingdojang/unit07/string_01.py
@@ -0,0 +1,14 @@
+single_quote = '''"안녕하세요."
+'파이썬'입니다.'''
+
+double_quote1 = """"Hello"
+'Python'"""
+
+double_quote2 = """Hello, 'Python'"""
+
+print(single_quote)
+print(double_quote1)
+print(double_quote2)
+
+print('Hello, \'Python\'')
+# 이처럼 문자열 안에 '," 등의 특수 문자를 포함하기 위해 앞에 \ 를 붙이는 방법을 이스케이프(escape)라 부른다.
