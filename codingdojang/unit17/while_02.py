449b4f14c26a500711147f967ed12bd721780c3f
Dayoming <jdayoung1204@naver.com>
2018-07-19 21:09:48 +0900

    Unit 17. while 반복문으로 Hello, world! 100번 출력하기

---
+++ codingdojang/unit17/while_02.py
@@ -0,0 +1,4 @@
+i = 100
+while i > 0:
+    print('Hello, world!', i)
+    i -= 1
---
+++ codingdojang/unit17/while_03.py
@@ -0,0 +1,9 @@
+count = int(input('반복할 횟수를 입력하세요: '))
+
+i = 0
+while i < count:
+    print('Hello, world! %d' % i)
+    i += 1
+
+# %d : 문자열 포맷 코드
+# 삽입할 숫자는 % 문자 다음에 써넣는다.
